6908a614a666d89c8917daf2e7220094e054aa53
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-05 16:17:56 +0530

    addition program

--- app.ts
+++ app.ts
@@ -1,1 +1,5 @@
-console.log("your code goes here...");
+function sum(num1: number, num2: number, phrase: string) {
+  const result = num1 + num2;
+  console.log(phrase, result);
+}
+sum(3, 4, "Result is: ");
