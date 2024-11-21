f881e0539bc9825650f96de8f7385ec250901e58
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-21 11:22:53 +0530

    intersection types

--- advanced types/src/app.ts
+++ advanced types/src/app.ts
@@ -1,1 +1,25 @@
-console.log("hello");
+// intersection types
+
+type Admin = {
+  name: String;
+  privileges: String[];
+};
+
+type Employee = {
+  name: String;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee; //it will combine properties of both types
+
+const el1: ElevatedEmployee = {
+  name: "elevated",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = String | Number;
+type Numeric = Number | Boolean;
+type Universal = Combinable & Numeric //it will act as intersection of both types
+
+const u1:Universal=12
